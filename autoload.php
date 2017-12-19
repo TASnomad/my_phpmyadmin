b5b9573a8eaf6f41a7fb71162facb9c297e03a78
ducks-skills <briand78410@gmail.com>
2017-12-19 22:08:58 +0100

    #14 autoload

--- autoload.php
+++ autoload.php
@@ -1,9 +1,8 @@
 <?php
-
 function __autoload($classname)
 {
-	if (file_exists(realpath($_SERVER['DOCUMENT_ROOT'].'/src/class/'.$classname.'.class.php')))
+	if (file_exists(realpath(dirname(__FILE__).'/src/class/'.$classname.'.class.php')))
 		require 'src/class/'.$classname.'.class.php';
 }
-
+//spl_autoload_register('');
 ?>
