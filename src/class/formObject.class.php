0d44eac23fcb03522f7f70596da5eef0576c5730
ducks-skills <briand78410@gmail.com>
2017-12-19 22:57:09 +0100

    #15 form ok

--- src/class/formObject.class.php
+++ src/class/formObject.class.php
@@ -31,7 +31,7 @@ class formObject
                         <h4 class='center'> Formulaire : $this->name </h4>\n";
            $str .= "    <form action='$this->co_link' method='$this->method>\n";
            foreach ($this->field as $field){
-               var_dump($field);
+               $str .= $this->getFieldAndLabel($field);
            }
            $str .= "    </div>\n
                         <div class='col s1'></div>\n
@@ -40,5 +40,46 @@ class formObject
         else
             $str = "Error while doing toString";
         return $str;
+    }
+
+    public function getFieldAndLabel($field){
+      $res = "";
+      $name = "";
+      $type = "";
+      if (isset($field['name'])){
+        $name = $field['name'];
+      }
+      else
+        $res = $this->getErrorInput();
+      if (isset($field['type'])){
+        $type = $field['type'];
+      }
+      else
+        $res = $this->getErrorInput();
+      if (isset($field['type'])){
+          $type = $field['type'];
+      }
+      else
+        $res = $this->getErrorInput();
+      if ($res == ""){
+        $res = "<div class='row'> \n
+                  <div class='col s3'></div>\n
+                  <div class='input-field col s6'> \n";
+        $res .= "   <input name='$name' id='$name' type = '$type'" . $this->getExtras() . " class='validate'>\n";
+        $res .= "   <label for='$name'>$name :</label>\n";
+        $res .= " <div class='col s3'></div>\n
+                  </div>\n
+                  </div>\n";
+      }
+      return $res;
+    }
+
+    public function getErrorInput()
+    {
+      return "<p class='red darken-3'>Erreur dans la création du formulaire !!</p>";
+    }
+
+    public function getExtras(){
+      return "";
     }
 }
