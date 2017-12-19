f9688438cbdee2cf49f95dd84ffc6a1ca36483fa
A Random User <martin.barreau@etu.u-pec.fr>
2017-12-19 21:49:36 +0100

    Fixing shitty commit

--- src/class/DBconnection.class.php
+++ src/class/DBconnection.class.php
@@ -1,25 +1,24 @@
 <?php
 
 class DBconnection {
-
 	private $dbString = "mysql:host=localhost;dbname=mysql";
 	private $login = "root";
 	private $password = "root";
 
-	private $instance = null;
+	private static $instance = null;
 
 	public static function getInstance() {
-		if ($instance == null) {
+		if (self::$instance == null) {
 			self::$instance = new DBconnection();
 		}
 		return (self::$instance);
 	}
 
 	private function __construct($throwError = true) {
-		$this->instance = new PDO($this->dbString, $this->login, $this->password);
+		self::$instance = new PDO($this->dbString, $this->login, $this->password);
 
-		if ($throwError && $this->instance) {
-			this->instance->setAttribute(PDO::ATTR_ERRMODE, PDO::EXECPTION_MODE);	
+		if ($throwError && self::$instance->instance) {
+			self::$instance->setAttribute(PDO::ATTR_ERRMODE, PDO::EXECPTION_MODE);
 		}
 	}
 }
--- src/view/index.php
+++ src/view/index.php
@@ -3,7 +3,7 @@
 	<head>
 		<?php
 		require_once("header.php");
-		require_once("../autoload.php");
+		require_once("../../autoload.php");
 		$test = DBconnection::getInstance();
 		var_dump($test);
 		//require_once("../class/formObject.class.php");
