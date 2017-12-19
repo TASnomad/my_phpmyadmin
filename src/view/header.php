6b5e7dc2d84ac88e0a11061794e88fddbb468d76
Corentin <Corentin@DESKTOP-FARB4O3>
2017-12-19 11:52:45 +0100

    #6 essai protetion header

--- src/view/header.php
+++ src/view/header.php
@@ -2,6 +2,13 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="description" content="my_phpmydadmin">
 <link rel="stylesheet" href="../css/stylesheet.css">
+
+<?php
+    session_start();
+    if(!isset($_SESSION['identifiant'])){
+        header("Location : ../view/index.php");
+    }
+?>
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js'></script>
