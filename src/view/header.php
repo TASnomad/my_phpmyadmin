d202872fae772fc8dcd4f5be3bf7d4895e563c94
Corentin <Corentin@DESKTOP-FARB4O3>
2017-12-19 11:22:41 +0100

    #3 test css

--- src/view/header.php
+++ src/view/header.php
@@ -1,7 +1,7 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="twitter analysis system">
-<link rel="stylesheet" href="<?php echo $domain_name; ?>/styles/materialize.min.css">
+<meta name="description" content="my_phpmydadmin">
+<link rel="stylesheet" href="../css/stylesheet.css">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
 <script src='https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.0/js/materialize.min.js'></script>
--- src/view/index.php
+++ src/view/index.php
@@ -5,4 +5,8 @@
             require_once("header.php")
        ?>
 	</head>	
+	
+	<body>
+		<h1> TEST </h1>
+	</body>
 </html>
