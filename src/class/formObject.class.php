fe7bb6d7f0ba9b35f401a46c767a2f49512487c6
Corentin <Corentin@DESKTOP-FARB4O3>
2017-12-19 14:36:43 +0100

    #7 ajout de la classe formObject et test de celle ci

---
+++ src/class/formObject.class.php
@@ -0,0 +1,41 @@
+<?php
+namespace src\class;
+
+class formObject
+{
+    //titre du formulaire 
+    private $name;
+    
+    //champs
+    private $field = [];
+    
+    //lien du controller =
+    private $co_link;
+    
+    //methode (POST par ex)
+    private $method;
+    
+    public function __construct($name = "unknown", $field = NULL, $co_link = NULL, $method = "POST"){
+        $this->name = $name;
+        $this->field = $field;
+        $this->co_link = $co_link;
+        $this->method = $method;
+    }
+    
+    protected function toString(){
+        $str = "";
+        if(isset($this))
+        {
+           $str .= "<h4> Formulaire : $this->name </h4>\n";
+           $str .= "    <form action='$this->co_link' method='$this->POST>\n";
+           foreach ($this->field as $field){
+               var_dump($field);
+           }
+           $str .= "    </form>\n";
+        }
+        else
+            $str = "Error while doing toString";
+        return $str;
+    }
+}
+
--- src/view/index.php
+++ src/view/index.php
@@ -1,11 +1,21 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<?php require_once("header.php"); ?>
+		<?php 
+		require_once("header.php"); 
+		require_once("../class/formObject.class.php");
+		?>
 	</head>	
 	
 	<body>
 		<?php require_once('../models/nav/nav.php'); ?>
 		<h1> TEST </h1>
+		<?php
+		$field = [];
+		$field['name'] = "test";
+		$field['type'] = "password";
+		$test = new formObject($test, [0 => $field], "lol", "POST");
+		$test->toString();
+		?>
 	</body>
 </html>
