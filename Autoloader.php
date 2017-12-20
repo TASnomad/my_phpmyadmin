c9c9481958676353b5e86237357150c1f78c42e6
Martin Barreau <martin.barreau@gmail.com>
2017-12-20 21:43:26 +0100

    Fixing autoloader

--- Autoloader.php
+++ Autoloader.php
@@ -1,25 +1,17 @@
 <?php
 
-/*class Autoloader
+class Autoloader
 {
 	public static function autoload($classname)
 	{
-		if (file_exists(realpath(dirname(__FILE__).'/src/class/'.$classname.'.php')))
-			require realpath(dirname(__FILE__).'/src/class/'.$classname.'.php');
+		if (file_exists(realpath(dirname(__FILE__).'/src/class/'.$classname.'.class.php')))
+			require realpath(dirname(__FILE__).'/src/class/'.$classname.'.class.php');
 	}
 
 	public static function register()
 	{
 		spl_autoload_register(array(__CLASS__ ,'autoload'));
 	}
-}*/
-
-
-function __autoload($classname)
-{
-	if (file_exists(realpath(dirname(__FILE__).'/src/class/'.$classname.'.class.php')))
-		require 'src/class/'.$classname.'.class.php';
 }
-//spl_autoload_register();
 
 ?>
--- src/view/index.php
+++ src/view/index.php
@@ -9,6 +9,10 @@
 		require_once("../models/forms/formSQLConsole.php");
 		$test = DB::getDBFields("user");
 		require_once("../class/formObject.class.php");
+			error_reporting(E_ALL | E_STRICT);
+			ini_set("display_errors", 1);
+			require_once("header.php");
+			require_once("../../Autoloader.php");
 		?>
 	</head>
 
