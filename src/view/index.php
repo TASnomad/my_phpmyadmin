46b7ac503ea8b56b4666c7f56dbe6c7339703417
Manuel <manuel.dambo@outlook.com>
2017-12-20 16:58:02 +0100

    Modification de autoload en Autoloader

--- src/view/index.php
+++ src/view/index.php
@@ -3,7 +3,7 @@
 	<head>
 		<?php
 		require_once("header.php");
-		require_once("../../autoload.php");
+		require_once("../../Autoloader.php");
 		require_once("../models/forms/formSQLConsole.php");
 		$test = DBconnection::getInstance();
 		require_once("../class/formObject.class.php");
@@ -17,13 +17,13 @@
 		$field = [];
 		$field['name'] = "login";
 		$field['type'] = "text";
-		
+
 		$f2 = [];
 		$f2['name'] = "password";
 		$f2['type'] = "password";
 		$test = new formObject("connexion", [0 => $field, 1 => $f2], "lol", "POST");
 		echo $test->toString();
-		
+
 		createSQLConsole();
 		?>
 	</body>
