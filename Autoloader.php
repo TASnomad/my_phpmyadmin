ec510f4785fa89f8ee98f9c40d3322f1dffdc23a
Manuel <manuel.dambo@outlook.com>
2017-12-20 15:56:47 +0100

    modification de l'Autoloder

---
+++ Autoloader.php
@@ -0,0 +1,25 @@
+<?php
+
+class Autoloader
+{
+	public static function autoload($classname)
+	{
+		if (file_exists(realpath(dirname(__FILE__).'/src/class/'.$classname.'.php')))
+			require realpath(dirname(__FILE__).'/src/class/'.$classname.'.php');
+	}
+
+	public static function register()
+	{
+		spl_autoload_register(array(__CLASS__ ,'autoload'));
+	}
+}
+
+
+/*function __autoload($classname)
+{
+	if (file_exists(realpath(dirname(__FILE__).'/src/class/'.$classname.'.class.php')))
+		require 'src/class/'.$classname.'.class.php';
+}
+//spl_autoload_register('');*/
+
+?>
--- autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-function __autoload($classname)
-{
-	if (file_exists(realpath(dirname(__FILE__).'/src/class/'.$classname.'.class.php')))
-		require 'src/class/'.$classname.'.class.php';
-}
-//spl_autoload_register('');
-?>
