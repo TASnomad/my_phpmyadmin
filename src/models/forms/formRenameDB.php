79e5cb0f112b16ed098e6dd548a44c36ef5d8feb
Manuel <manuel.dambo@outlook.com>
2017-12-21 16:39:39 +0100

    Formulaire rename d'une DB

---
+++ src/models/forms/formRenameDB.php
@@ -0,0 +1,18 @@
+<?php
+require_once("../../Autoloader.php");
+
+function createDB($error = ""){
+  $filed = [];
+  $field['name'] = "Nom de la base";
+  $field['type'] = "text";
+  $field['error'] = $error;
+  $field['placeholder'] = 'Entrez le nom de la base à renommer';
+
+  $filed2 = [];
+  $field2['name'] = "Nouveau nom de la base";
+  $field2['type'] = "text";
+  $field2['placeholder'] = 'Entrez le nouveau nom de la base';
+  $form = new formObject('Renommer une base', [0 => $field, 1 => $field2], '../controllers/.php');
+  echo $form->toString();
+}
+?>
