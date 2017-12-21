fb659d75f1779ab1a95c85d070966085d5091d3b
Manuel <manuel.dambo@outlook.com>
2017-12-21 16:39:05 +0100

    Modification du formulaire de création d'une DB

--- src/models/forms/formCreateDB.php
+++ src/models/forms/formCreateDB.php
@@ -7,7 +7,7 @@ function createDB($error = ""){
   $field['type'] = "text";
   $field['error'] = $error;
   $field['placeholder'] = 'Entrez le nom de la base à créer';
-  $form = new formObject('', [0 => $text], '../controllers/.php');
+  $form = new formObject('Créer une base', [0 => $field], '../controllers/.php');
   echo $form->toString();
 }
 ?>
