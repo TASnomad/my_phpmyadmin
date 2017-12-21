1ab6831fd84b9a1b2ee345779270a349e72e2661
Corentin <Corentin@DESKTOP-FARB4O3>
2017-12-21 16:58:28 +0100

    #28 ow god i'm so stupid

--- src/controllers/consoleController.php
+++ src/controllers/consoleController.php
@@ -3,7 +3,7 @@ if (isset($_POST['console_SQL']) && $_POST['console_SQL'] != ''){
   if (!preg_match("/\"/", $_POST['console_SQL'])){
     $sql = $_POST['console_SQL'];
     $query = new queryTry($_POST['console_SQL']);
-    query->execute();
+    $query->execute();
   }
   else {
     header("Location: ../view/console_SQL.php?err=format invalide");
