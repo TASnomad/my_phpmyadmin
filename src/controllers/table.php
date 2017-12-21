655e1c4dea12966ef1a69dbd1c0c3381bcb7aa20
Manuel <manuel.dambo@outlook.com>
2017-12-22 00:35:54 +0100

    début controllers table

---
+++ src/controllers/table.php
@@ -0,0 +1,11 @@
+<?php
+	error_reporting(E_ALL | E_STRICT);
+	ini_set("display_errors", 1);
+
+	require '../../Autoloader.php';
+	Autoloader::register();
+
+	$protocol  = empty($_SERVER['HTTPS']) ? 'http' : 'https';
+	$fullUrl  = "${protocol}://". $_SERVER['HTTP_HOST']."/src/view";
+
+  ?>
